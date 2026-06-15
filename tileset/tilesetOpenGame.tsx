03d3e91dac7977332fdd0298ac185abcb28701c8
paulooventura <paulooventura@gmail.com>
2026-06-14 19:29:52 -0500

    Initial commit

---
+++ tileset/tilesetOpenGame.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="tilesetOpenGame" tilewidth="8" tileheight="8" tilecount="960" columns="40">
+ <image source="tilesetOpenGame.png" trans="ffffff" width="320" height="192"/>
+</tileset>
